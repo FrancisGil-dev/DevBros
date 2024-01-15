c29cb410cbbd9420267224560feb7d33e4398dbb
Francis.dev <149237151+FrancisGil-dev@users.noreply.github.com>
2024-01-15 21:05:58 +0800

    commit

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -2,15 +2,14 @@
 import './globals.css';
 
 
-
 export const metadata = {
   title: 'DevBros || Portfolio',
 }
 
 export default function RootLayout({ children }) {
-  return (
+  return(
     <html lang="en">
-      <body>{children}</body>
+      <body className='bg-[#0a192f] text-gray-300'>{children}</body>
     </html>
   )
 }
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,8 +1,9 @@
-
-export default function Home() {
+import React from "react"
+import Home from "./components/Home"
+export default function Root() {
   return (
     <>
-      
+      <Home/>
     </>
   )
 }
